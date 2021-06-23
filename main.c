8de3cfea933dba06d6dc2169f9ff7e94831b2634
Mouaad Lasritene <mlasrite@e0r4p3.1337.ma>
2021-06-23 14:05:21 +0100

    .

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: mlasrite <mlasrite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 12:32:18 by mlasrite          #+#    #+#             */
-/*   Updated: 2021/06/23 13:24:33 by mlasrite         ###   ########.fr       */
+/*   Updated: 2021/06/23 14:04:54 by mlasrite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ int error_handling(int argc, char **argv)
 {
     int i;
 
-    i = 0;
+    i = 1;
     while (i < argc)
     {
         if (ft_isdigit(argv[i]) == 1)
@@ -31,7 +31,7 @@ int main(int argc, char **argv)
     int ret;
 
     ret = 0;
-    if (argc == 4 || argc == 5)
+    if (argc == 5 || argc == 6)
     {
         ret = error_handling(argc, argv);
         if (ret == 1)
--- philo.h
+++ philo.h
@@ -6,13 +6,14 @@
 /*   By: mlasrite <mlasrite@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/06/23 13:15:51 by mlasrite          #+#    #+#             */
-/*   Updated: 2021/06/23 13:18:12 by mlasrite         ###   ########.fr       */
+/*   Updated: 2021/06/23 14:04:34 by mlasrite         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PHILO_H
 #define PHILO_H
 #include <unistd.h>
+#include <stdio.h>
 
 int ft_isdigit(char *str);
 
